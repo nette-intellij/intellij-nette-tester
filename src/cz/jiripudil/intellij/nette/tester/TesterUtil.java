1e7a2635057c7dc717593f871dbe1e75451b1ec2
Daniel Kurecka <danielkurecka@gmail.com>
2017-07-23 20:26:43 +0200

    Add utility method for finding doc. comment that is analyzed by Tester

--- src/cz/jiripudil/intellij/nette/tester/TesterUtil.java
+++ src/cz/jiripudil/intellij/nette/tester/TesterUtil.java
@@ -2,11 +2,16 @@ package cz.jiripudil.intellij.nette.tester;
 
 import com.intellij.openapi.util.Ref;
 import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.util.PsiTreeUtil;
 import com.jetbrains.php.PhpClassHierarchyUtils;
 import com.jetbrains.php.lang.PhpLangUtil;
+import com.jetbrains.php.lang.documentation.phpdoc.psi.PhpDocComment;
 import com.jetbrains.php.lang.psi.elements.Method;
 import com.jetbrains.php.lang.psi.elements.PhpClass;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 
 public class TesterUtil {
     public static boolean isTestClass(@NotNull PhpClass phpClass) {
@@ -32,5 +37,20 @@ public class TesterUtil {
             && isTestClass(method.getContainingClass())
             && StringUtil.startsWith(method.getName(), "test")
             && method.getModifier().isPublic();
+    }
+
+    /**
+     * Returns a doc. comment from which Nette Tester reads annotations.
+     * Nette Tester reads them from first unindented doc. comment.
+     * @see <a href="https://github.com/nette/tester/blob/v1.7.1/src/Framework/Helpers.php#L42">Comment parsing</a>
+     */
+    @Nullable
+    public static PhpDocComment findDocCommentRedByTester(PsiFile file) {
+        for (PhpDocComment comment: PsiTreeUtil.findChildrenOfType(file, PhpDocComment.class)) {
+            if (comment.getPrevSibling().getText().endsWith("\n")) {
+                return comment;
+            }
+        }
+        return null;
     }
 }
